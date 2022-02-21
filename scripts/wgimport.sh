1a84ed15ee6c4f5ba2e433c08b68a5fd7843bf6b
brian-anderson01 <brian.anderson01@mymail.champlain.edu>
2022-02-21 11:52:28 -0500

    updated wgimport

--- scripts/wgimport.sh
+++ scripts/wgimport.sh
@@ -1,2 +1,2 @@
 outfile=$(yad --width 600 --height 400 --on-top --center --no-escape --file --title="Select the config you would like to import")
-#cp $outfile "/path/to/wireguard-config-folder"
+cp $outfile "/etc/wireguard/wg0.conf"

cfb439fee7daa8ab80f72392b48aca7e7462acdf
brian-anderson01 <brian.anderson01@mymail.champlain.edu>
2022-02-21 13:35:35 -0500

    updated scripts

--- scripts/wgimport.sh
+++ scripts/wgimport.sh
@@ -1,2 +1,10 @@
 outfile=$(yad --width 600 --height 400 --on-top --center --no-escape --file --title="Select the config you would like to import")
 cp $outfile "/etc/wireguard/wg0.conf"
+
+if test -f "/etc/wireguard/wg0.conf"; then
+    yad --width 300 --height 100 --on-top --center --title "Successfully Imported" \
+    --text "WireGuard config was sucessfully imported"
+else
+    yad --width 300 --height 100 --on-top --center --title "Failed to import config" \
+    --text "WireGuard config failed to import. There may be an issue with the permissions of your Wireguard directory."
+fi
